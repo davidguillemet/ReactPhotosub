4f05d959f7e5fa09a3c4dffc345465b86ad2b28a
David Guillemet <david.guillemet@hotmail.com>
2021-03-05 19:35:19 +0100

    first version of search api

--- functions/api/expressApi.js
+++ functions/api/expressApi.js
@@ -134,6 +134,29 @@ app.route("/image")
         }
     });
 
+
+// Search for images
+app.route("/search")
+    .post(async function(req, res, next) {
+        const searchData = req.body;
+        const page = searchData.page;
+        const query = searchData.query;
+        const pageSize = searchData.pageSize;
+        pool("images")
+            .select("name", "path", "title", "description")
+            .limit(pageSize).offset(page * pageSize)
+            // title is written in french
+            // description in english, but in franch "manta" is extracted as lexeme "mant"...
+            // on the other hand stopwork as "the" or "le" are ignored only if we use a config "english" or "french"...
+            .whereRaw(`to_tsvector('french', title) @@ to_tsquery('${query}')`)
+            .then((results) => {
+                res.json(results);
+            }).catch((err) => {
+                logger.error(`Failed to search images from query "${query}"`, err);
+                res.status(500).send("Error while searching image.").end();
+            });
+    });
+
 app.get("/status", (req, res) => res.send("Working!"));
 
 const mainapi = express();
