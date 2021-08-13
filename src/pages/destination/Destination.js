fa03c8a64a9e432296b6d88bd403495f6c44da7e
David Guillemet <david.guillemet@hotmail.com>
2021-08-13 23:48:22 +0200

    Add tags as destination regions

--- src/pages/destination/Destination.js
+++ src/pages/destination/Destination.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Box from '@material-ui/core/Box';
+import Chip from '@material-ui/core/Chip';
 import { formatDate } from '../../utils/utils';
 import dataProvider from '../../dataProvider';
 import { Gallery } from '../../components';
@@ -8,6 +10,28 @@ import PageTitle, { PageSubTitle } from '../../template/pageTitle';
 
 import 'fontsource-roboto/400.css';
 import 'fontsource-roboto/100.css';
+
+
+const RegionChip = ({region}) => {
+
+    return (
+        <Chip label={region.title} sx={{m: 1}} />
+    )
+}
+
+const RegionPath = ({regions}) => {
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+            }}
+        >
+        { regions.map(region => <RegionChip key={region.id} region={region} />) }
+        </Box>
+    );
+} 
 
 const Destination = () => {
     const { year, title } = useParams();
@@ -36,6 +60,7 @@ const Destination = () => {
         <React.Fragment>
             <PageTitle>{destination.title}</PageTitle>
             <PageSubTitle>{formatDate(new Date(destination.date))}</PageSubTitle>
+            <RegionPath regions={destination.region_path}></RegionPath>
             <Gallery images={images} style={{width: '100%'}} colWidth={300} margin={5}/>
         </React.Fragment>
     );
