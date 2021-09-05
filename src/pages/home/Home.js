f6d99c588f9e7b499fcba30164be15bfed7d93e9
David Guillemet <david.guillemet@hotmail.com>
2021-09-05 18:15:29 +0200

    use -webkit-transition

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -116,7 +116,7 @@ const Home = () => {
             <TransitionGroup component={null}>
                 <CSSTransition
                     key={currentImageIndex}
-                    timeout={2500}
+                    timeout={2000}
                     classNames="slide"
                 >
                     <Box
