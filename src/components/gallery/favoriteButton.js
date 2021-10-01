b3e57ff2acea864f0896bc18984658c9314f6f0c
David Guillemet <david.guillemet@hotmail.com>
2021-10-01 23:44:06 +0200

    Favorite button with loading

--- src/components/gallery/favoriteButton.js
+++ src/components/gallery/favoriteButton.js
@@ -1,4 +1,4 @@
-import React, { useMemo} from 'react';
+import React, { useMemo, useState } from 'react';
 import FavoriteIconOutlined from '@material-ui/icons/FavoriteBorderOutlined';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import Typography from '@material-ui/core/Typography';
@@ -7,16 +7,19 @@ import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 
 import TooltipIconButton from '../tooltipIconButton';
 import { useAuthContext } from '../authentication';
+import { CircularProgress } from '@material-ui/core';
 
 const FavoriteButton = ({image, fontSize = 'default', style, color }) => {
 
     const path = `${image.path}/${image.name}`;
     const authContext = useAuthContext();
+    const [ updating, setUpdating ] = useState(false);
 
     // TODO: create isInfavorites() method in authContext
     const isInFavorites = useMemo(() => authContext.data && authContext.data.favorites.has(path), [authContext.data, path]);
 
     function handleFavoriteClick() {
+        setUpdating(true);
         const favoriteActionPromise =
             isInFavorites ?
             authContext.removeUserFavorite(image) :
@@ -24,6 +27,8 @@ const FavoriteButton = ({image, fontSize = 'default', style, color }) => {
 
         favoriteActionPromise.catch(err => {
             console.error(err);
+        }).finally(() => {
+            setUpdating(false);
         })
     }
 
@@ -60,6 +65,7 @@ const FavoriteButton = ({image, fontSize = 'default', style, color }) => {
             }
             onClick={authContext.user ? handleFavoriteClick : null}
             style={buttonStyle}
+            disabled={updating}
         >
             {
                 isInFavorites ?
