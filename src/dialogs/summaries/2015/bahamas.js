8e7c8f164b7ad3677bf55c1eec9ebdd4b297614f
David Guillemet <david.guillemet@hotmail.com>
2021-11-07 22:38:10 +0100

    new summaries

---
+++ src/dialogs/summaries/2015/bahamas.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import SummaryParagraph from '../../SummaryParagraph';
+import HtmlList from '../../HtmlList';
+import ListItem from '@mui/material/ListItem';
+
+const ambon = () => (
+<React.Fragment>
+<SummaryParagraph>Après des repérages un an plus tôt, Steven Surina nous a préparé une petite expédition sur mesure pour aller à la rencontre des requins des Bahamas. C'est donc sous la houlette de <a href="http://www.sharkeducation.com/" target="_blank" rel="noreferrer">Shark Education</a>, en partenariat avec <a href="http://www.h2ovoyage.com/" target="_blank" rel="noreferrer">H2O Voyage</a> et <a href="http://chercheursdeau.com/" target="_blank" rel="noreferrer">Chercheurs d'Eau</a> que nous nous envolons pour Nassau en ce début de Février 2015</SummaryParagraph>
+<SummaryParagraph>Pour la première étape de notre séjour, nous allons passer une journée au centre de plongée de <a href="http://www.stuartcove.com/" target="_blank" rel="noreferrer">Stuart Cove</a> avec pas moins de 4 plongées, dont deux seront consacrées à des séances de feeding.</SummaryParagraph>
+<SummaryParagraph>Cette pratique quelque peu décriée consiste à attirer les requins avec une boîte remplie de morceaux de poissons, le plus souvent en état de décomposition légèrement avancée, et à l'occasion, à impressionner les plongeurs en les nourrissant.</SummaryParagraph>
+<SummaryParagraph>S'en suit une légère frénésie, les requins étant alors dans un certain état d'excitation, mais jamais agressifs.</SummaryParagraph>
+<SummaryParagraph>Il s'agit ici d'un pur show à l'américaine, avec un dive master spécialisé, protégé d'un cotte de maille et accompagné d'un photographe qui immortalisera votre plongée au milieu de la mêlée...</SummaryParagraph>
+<SummaryParagraph>Dés le lendemain matin, nous nous envolons pour l'Île de Grand Bahama - au milieu d'un transfert de prisonniers - où nous logerons dans un "All inclusive" près de Freeport.</SummaryParagraph>
+<SummaryParagraph>Pour cette deuxième journée, nous allons aller à la rencontre de Cristina Zenato, pionnière de l'immobilité tonique, et qui a développé une véritable relation avec les requins, quelle considère tout simplement comme ses "bébés".</SummaryParagraph>
+<SummaryParagraph>Ici, même si Cristina plonge également en cotte de maille, accompagnée d'un photographe (pratique très anglo saxonne) nous ne sommes plus dans le show pur. Elle ne nourrit pas les requins, elle ne fait que les attirer avec un tube rempli de poissons, mais il n'y a donc pas de réelle frénésie. Elle prend sont temps, les caresse, jusqu'à trouver celui qui va être le plus réceptif ce jour-là pour l'emmener jusqu'à cet état d'immobilité tonique. Le requin va être comme hypnotisé et va se laisser porter et positionner à la verticale sans bouger...impressionnant !</SummaryParagraph>
+<SummaryParagraph>Il est particulièrement touchant de voir Cristina à genou dans le sable, en train de caresser la tête d'un requin posée sur sa cuisse, ce même requin revenant se mettre en position et réclamer des caresses lorsque Cristina se recule...</SummaryParagraph>
+<SummaryParagraph>Si jusqu'ici nous n'avons observé que des requins gris (mis à part un petit tigre qui est timidement passé lors d'une séance de feeding à Nassau), nous allons maintenant passer au niveau supérieur, avec le requin tigre...</SummaryParagraph>
+<SummaryParagraph>Pour les 3 jours qui suivent, nous devons donc nous rendre en bus très tôt le matin à West End, à l'extrémité ouest de l'Île de Grand Bahama, pour partir plonger au large, sur Tiger Beach, la bien nommée...</SummaryParagraph>
+<SummaryParagraph>Malheureusement, en raison d'un vent assez soutenu, la première journée a été tout simplement annulée.</SummaryParagraph>
+<SummaryParagraph>Le deuxième jour nous avons tout de même fait le déplacement, allant jusqu'à écouter le briefing de Beto, notre guide, et même jusqu'à sortir de la marina en bateau, mais il s'agissait plus de nous démontrer que la houle était encore trop forte...</SummaryParagraph>
+<SummaryParagraph>Le troisième jour sera finalement le bon, mais il aura quand même fallu attendre plus d'une heure, par 10m de fond, que le premier tigre daigne se montrer.</SummaryParagraph>
+<SummaryParagraph>La première rencontre avec ce squale impressionne, et encore plus lorsque celui-ci décide de venir droit sur vous jusqu'à "taper" le dôme de votre appareil photo. Mais aucune agressivité ou nervosité ne se fait sentir. Il n'est intéressé que par la "Bait Box", cette boîte métallique remplie de ce fameux mélange de poissons pourris. Nous ne faisons même plus attention aux requins citron et requin gris qui nous tournent autour et n'avons d'yeux que pour le Tigre, que nous devons d'ailleurs ne jamais perdre de vue, par mesure de sécurité...</SummaryParagraph>
+<SummaryParagraph>Ici, la plongée s'organise de manière un peu particulière. Nous ne sommes pas profonds et dès que la panne d'air approche nous remontons sur le bateau en nous frayant un chemin au milieu des requins citron, buvons un verre d'eau, changeons de bouteille et redescendons. Simple et efficace :)</SummaryParagraph>
+<SummaryParagraph>Au total, nous avons dû passer plus de 3h de suite dans l'eau...</SummaryParagraph>
+<SummaryParagraph>La dernière étape de notre voyage va se dérouler à Bimini, pour aller rencontrer le grand requin marteau, le Mokarran.</SummaryParagraph>
+<SummaryParagraph>Le site où nous allons plonger se situe juste devant l'île de Bimini, donc même si la houle se lève, et contrairement à Tiger Beach, nous sommes sûr de plonger.</SummaryParagraph>
+<SummaryParagraph>Cette fois, nous attendons sur le bateau, pendant que Beto appâte les marteaux, facilement repérables lorsqu'ils évoluent sur ce fond sableux de moins de 10m.</SummaryParagraph>
+<SummaryParagraph>Normalement le consignes sont claires. Quand les marteaux sont là, il descend en premier, défini un ligne dans le sable derrière laquelle nous devons rester pendant qu'il se tient derrière tout en continuant à les attirer. De cette façon, le spectacle doit être total et permettre à tout le monde d'en profiter.</SummaryParagraph>
+<SummaryParagraph>C'était sans compter sur 10 frenchies indisciplinés et assoiffés de sensations fortes:
+<HtmlList>
+    <ListItem>Une ligne? où ça?</ListItem>
+    <ListItem>Ben moi je vais là, c'est mieux...</ListItem>
+    <ListItem>Oh et puis non, finalement là-bas c'est mieux...</ListItem>
+</HtmlList>
+En gros, une belle cacophonie sous l'eau avec des mouvements dans tous les sens et les jolis nuages de sable qui vont avec :)
+</SummaryParagraph>
+<SummaryParagraph>Tant pis pour les photos...qui je dois l'avouer ont pour beaucoup nécessité un désensablage un peu poussé...</SummaryParagraph>
+<SummaryParagraph>Quoi qu'il en soit, ces animaux sont absolument extraordinaires, parfois un peu plus nerveux que les tigres, mais dotés d"une souplesse impressionnante et d'un élégance incomparable...</SummaryParagraph>
+</React.Fragment>
+)
+
+export default ambon;
