3ffb49437cf8a25b1c7d09474f0546d4acf9db36
David Guillemet <david.guillemet@hotmail.com>
2021-11-12 18:50:59 +0100

    Social Icons

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import Footer from './template/footer';
 import ScrollTop from './template/scrollTop';
 import PageContent from './template/pageContent';
 import ResponsiveTheme from './template/theme';
+import SocialIcons from './components/socialIcons';
 
 import './App.css';
 
@@ -36,207 +37,209 @@ const drawerWidth = 240;
 const scrollTopAnchor = "back-to-top-anchor";
 
 const Main = styled('main', {
-  shouldForwardProp: (prop) => prop !== 'open'
+    shouldForwardProp: (prop) => prop !== 'open'
 })(({ theme, open }) => {
-  return {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-    flexGrow: 1,
-    paddingBottom: 0,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-    overflow: 'hidden',
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  }}
-);
+    return {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        flexGrow: 1,
+        paddingBottom: 0,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+        overflow: 'hidden',
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        }),
+    }
+});
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-start',
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
 }));
 
-const TopToolBar = withRouter(({open, handleDrawerOpen, location}) => {
+const TopToolBar = withRouter(({ open, handleDrawerOpen, location }) => {
 
-  const transparent = location.pathname === '/';
+    const transparent = location.pathname === '/';
 
-  return (
-    <AppBar
-      position="fixed"
-      sx={{ 
-        width: '100%',
-        ...(transparent && { backgroundColor: 'rgba(0,0,0,0.2)' })
-      }}
-    >
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-          sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          size="large">
-          <MenuIcon />
-        </IconButton>
-        <FirebaseSignin />
-      </Toolbar>
-    </AppBar>
-  );    
+    return (
+        <AppBar
+            position="fixed"
+            sx={{
+                width: '100%',
+                ...(transparent && { backgroundColor: 'rgba(0,0,0,0.2)' })
+            }}
+        >
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                    size="large">
+                    <MenuIcon />
+                </IconButton>
+                <FirebaseSignin />
+            </Toolbar>
+        </AppBar>
+    );
 })
 
 function App(props) {
 
-  const [open, setOpen] = React.useState(false);
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setMobileOpen(true);
-  };
+    const handleDrawerOpen = () => {
+        setMobileOpen(true);
+    };
 
-  const handleDrawerClose = () => {
-    setMobileOpen(false);
-  };
-  
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const handleDrawerClose = () => {
+        setMobileOpen(false);
+    };
 
-  const handleHistoryChanged = () => {
-    handleDrawerClose();
-  }
-  
-  function isLinkActive(match, location) {
-    if (match === null) {
-      return false;
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const handleHistoryChanged = () => {
+        handleDrawerClose();
     }
 
-    if (match.url === "/" || match.url === "") {
-      return location.pathname === "/";
+    function isLinkActive(match, location) {
+        if (match === null) {
+            return false;
+        }
+
+        if (match.url === "/" || match.url === "") {
+            return location.pathname === "/";
+        }
+
+        if (location.pathname.indexOf(match.url) === 0) {
+            return true;
+        }
+
+        return false;
     }
 
-    if (location.pathname.indexOf(match.url) === 0) {
-      return true;
-    }
-
-    return false;    
-  }
-
-  const drawerContent = (
-    <div>
-      <DrawerHeader>
-        <IconButton
-          color="inherit"
-          aria-label="close drawer"
-          onClick={handleDrawerClose}
-          edge="start"
-          sx={{ ml: 1 }}
-          size="large">
-          <CloseIcon />
-        </IconButton>
-      </DrawerHeader>
-    <Divider variant="middle" />
-    <Box sx={{
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center'
-    }}>
-      <img src={logo} className="logo" alt="logo" />
-    </Box>
-    <List style={{width: '100%', marginTop: 30, marginBottom: 30}}>
-    {
-      routes.filter(route => route.sidebar).map((route, index) => {
-        return (
-          <NavigationLink key={index} to={route.path} isActive={isLinkActive}>
-            <ListItem button>
-              <ListItemIcon>
-                {route.icon}
-              </ListItemIcon>
-              <ListItemText primary={route.label} />
-            </ListItem>
-          </NavigationLink>
-        );
-      })
-    }
-    </List>
-    </div>
-  );
-
-  const container = window !== undefined ? () => window.document.body : undefined;
-
-  return (
-    <div style={{ display: 'flex'}}>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={ResponsiveTheme}>
-      <ReactQueryClientProvider>
-      <GlobalContextProvider>
-      <AuthProvider>
-
-        <CssBaseline />
-
-        <Router>
-
-          <TopToolBar open={open} handleDrawerOpen={handleDrawerOpen} />
-
-          <nav style={{
-                width: drawerWidth,
-                flexShrink: 0
-            }}
-          >
-          <Hidden smUp implementation="css">
-            <SwipeableDrawer
-              disableBackdropTransition={!isIOS}
-              disableDiscovery={isIOS}
-              container={container}
-              variant="temporary"
-              anchor="left"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              onOpen={handleDrawerToggle}
-              sx={{
-                '& .MuiPaper-root': {
-                  width: drawerWidth
+    const drawerContent = (
+        <div>
+            <DrawerHeader>
+                <IconButton
+                    color="inherit"
+                    aria-label="close drawer"
+                    onClick={handleDrawerClose}
+                    edge="start"
+                    sx={{ ml: 1 }}
+                    size="large">
+                    <CloseIcon />
+                </IconButton>
+            </DrawerHeader>
+            <Divider variant="middle" />
+            <Box sx={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center'
+            }}>
+                <img src={logo} className="logo" alt="logo" />
+            </Box>
+            <List sx={{ width: '100%', myt: 3, mb: 1 }}>
+                {
+                    routes.filter(route => route.sidebar).map((route, index) => {
+                        return (
+                            <NavigationLink key={index} to={route.path} isActive={isLinkActive}>
+                                <ListItem button>
+                                    <ListItemIcon>
+                                        {route.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={route.label} />
+                                </ListItem>
+                            </NavigationLink>
+                        );
+                    })
                 }
-              }}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-            >
-              {drawerContent}
-            </SwipeableDrawer>
-          </Hidden>
-          </nav>
+            </List>
+            <Divider variant="middle" />
+            <SocialIcons />
+        </div>
+    );
 
-          <Main open={open}>
-            <div id={scrollTopAnchor} />
-            <DrawerHeader />
-            <PageContent onHistoryChanged={handleHistoryChanged}/>
-            <Footer />
-          </Main>
+    const container = window !== undefined ? () => window.document.body : undefined;
 
-        </Router>
+    return (
+        <div style={{ display: 'flex' }}>
+            <StyledEngineProvider injectFirst>
+                <ThemeProvider theme={ResponsiveTheme}>
+                    <ReactQueryClientProvider>
+                        <GlobalContextProvider>
+                            <AuthProvider>
 
-        <ScrollTop {...props} anchorSelector={`#${scrollTopAnchor}`}></ScrollTop>
+                                <CssBaseline />
 
-      </AuthProvider>
-      </GlobalContextProvider>
-      </ReactQueryClientProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
-    </div>
-  );
+                                <Router>
+
+                                    <TopToolBar open={open} handleDrawerOpen={handleDrawerOpen} />
+
+                                    <nav style={{
+                                        width: drawerWidth,
+                                        flexShrink: 0
+                                    }}
+                                    >
+                                        <Hidden smUp implementation="css">
+                                            <SwipeableDrawer
+                                                disableBackdropTransition={!isIOS}
+                                                disableDiscovery={isIOS}
+                                                container={container}
+                                                variant="temporary"
+                                                anchor="left"
+                                                open={mobileOpen}
+                                                onClose={handleDrawerToggle}
+                                                onOpen={handleDrawerToggle}
+                                                sx={{
+                                                    '& .MuiPaper-root': {
+                                                        width: drawerWidth
+                                                    }
+                                                }}
+                                                ModalProps={{
+                                                    keepMounted: true, // Better open performance on mobile.
+                                                }}
+                                            >
+                                                {drawerContent}
+                                            </SwipeableDrawer>
+                                        </Hidden>
+                                    </nav>
+
+                                    <Main open={open}>
+                                        <div id={scrollTopAnchor} />
+                                        <DrawerHeader />
+                                        <PageContent onHistoryChanged={handleHistoryChanged} />
+                                        <Footer />
+                                    </Main>
+
+                                </Router>
+
+                                <ScrollTop {...props} anchorSelector={`#${scrollTopAnchor}`}></ScrollTop>
+
+                            </AuthProvider>
+                        </GlobalContextProvider>
+                    </ReactQueryClientProvider>
+                </ThemeProvider>
+            </StyledEngineProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/socialIcons/index.js
@@ -0,0 +1,2 @@
+export * from './socialIcons';
+export { default } from './socialIcons';
