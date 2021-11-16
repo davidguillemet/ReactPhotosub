0f2e41a691a46f90e5d3fdbabbba4a4d21c7753c
David Guillemet <david.guillemet@hotmail.com>
2021-11-16 22:08:52 +0100

    Query to create a destination view including full region path...

--- functions/api/resources/destinations.js
+++ functions/api/resources/destinations.js
@@ -23,6 +23,28 @@ module.exports = function(config) {
                 });
         });
 
+    // To compare the complete region path instead of the location region only,
+    // We can create a view that contains all destinations with the full region path as an array of region identifier
+    // -> but doing this is equivalent as checking of the highest level region is the same or not...
+    // DROP VIEW IF EXISTS destinationwithregionpath;
+    // CREATE VIEW destinationwithregionpath AS (
+    //     SELECT destinations.*, (
+    //         WITH RECURSIVE regionpath AS (
+    //                 SELECT
+    //                 reg.id, reg.title, reg.parent
+    //                 FROM regions reg
+    //                 WHERE reg.id = locations.region
+    //                 UNION ALL
+    //                 SELECT r.id, r.title, r.parent
+    //                 FROM regions r
+    //                 JOIN regionpath ON r.id = regionpath.parent
+    //             )
+    //         SELECT ARRAY(select id from regionpath)
+    //     ) as regionpath
+    //     from locations, destinations
+    //     where locations.id = destinations.location
+    // );
+    // select * from destinationwithregionpath;
     config.app.route("/destinations/related")
         .get(function(req, res, next) {
             const region = req.query.region;
