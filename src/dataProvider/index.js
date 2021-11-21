5f24f1dbd2a8327c663f3464a623f49aa73bcb7f
David Guillemet <david.guillemet@hotmail.com>
2021-11-21 16:52:57 +0100

    Update manifest.json + new favicons

--- src/dataProvider/index.js
+++ src/dataProvider/index.js
@@ -1,6 +1,5 @@
 import DataProvider from './dataprovider';
-import mockProvider from './mockprovider';
 import {isFromDb, isDirty, getDbIndex, TRANSIENT_PROPERTY_IS_DIRTY, TRANSIENT_PROPERTY_DB_INDEX} from './common';
 
-export default (process.env.REACT_APP_USE_MOCK === 'true') ? mockProvider : DataProvider;
+export default (process.env.REACT_APP_USE_MOCK === 'true') ? DataProvider : DataProvider;
 export {isFromDb, isDirty, getDbIndex, TRANSIENT_PROPERTY_IS_DIRTY, TRANSIENT_PROPERTY_DB_INDEX};
