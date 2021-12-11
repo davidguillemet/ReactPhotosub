6ad43d586015a1bc0871af435717ed2153f51ad1
David Guillemet <david.guillemet@hotmail.com>
2021-12-11 18:14:13 +0100

    Modify About text

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -15,13 +15,13 @@ const About = () => {
             <Paragraph>Il m'aura fallu un peu de temps pour devenir complètement accro à la plongée sous-marine...</Paragraph>
             <Paragraph>Un premier baptême en Australie en 2003, suivi d'un deuxième en Croatie un an plus tard, mais toujours pas le déclic...</Paragraph>
             <Paragraph>J'ai finalement passé mon niveau 1 un peu par hasard lors d'un séjour en Martinique en mars 2005, mais c'est réellement début 2006, aux Bahamas, lorsque ma route croise celle d'un requin marteau que tout va changer...</Paragraph>
-            <Paragraph>La plongée devient alors une véritable passion et je passe très rapidement mon niveau 2 avant de m'inscrire à l'ASD12 pour continuer ma progression (Nitrox, N3, N4, initiateur).</Paragraph>
+            <Paragraph>La plongée devient alors une véritable passion et je passe très rapidement mon niveau 2 en mai 2006 pour continuer ma progression dans la foulée vers le N3, puis le N4 en juin 2008, et enfin l'initiateur un an plus tard.</Paragraph>
             <Paragraph>En parallèle de mes niveaux techniques, je m'intéresse également très tôt à la biologie sous-marine et lors de cette première saison 2006-2007, je vais suivre les cours du Comité Départemental de Paris qui me mèneront à la validation de l'AFBS (Animateur Fédéral de Biologie Sous-marine - équivalent de l'actuel N1 Bio) lors d'une semaine de stage à Cerbère avec Robert Oms.</Paragraph>
             <Paragraph>De fil en aiguille, après la bio, je vais naturellement m'intéresser à la photo sous-marine...Et me voilà donc parti aux Maldives en février 2008, équipé d'un Nikon P5100 en caisson Ikelite, avec un flash DS125 de la même marque.</Paragraph>
-            <Paragraph>Les premiers résultats sont plutôt encourageants et au fur et à mesure de mon apprentissage des réglages manuels de l'appareil, je ressens une certaine frustration en raison de leur limitation.</Paragraph>
-            <Paragraph>Je passe donc au reflex en juillet 2009, et plus précisément au Nikon D90 en caisson Aquatica, avec mon fidèle flash DS125, auquel je vais associer un DS160, plus recent.</Paragraph>
-            <Paragraph>Entre temps, ce site est tout simplement né du désir de partager cette passion avec famille et amis, et c'est en septembre 2021 que cette 6ème version voit le jour...</Paragraph>
-            <Paragraph>Après 5 ans passés avec mon D90, j'ai ensuite évolué vers le D7100 puis le D7200, toujours en caisson Aquatica, avec mes deux flashs Ikelite DS125 et DS160.</Paragraph>
+            <Paragraph>Les premiers résultats sont plutôt encourageants et au fur et à mesure de mon apprentissage des réglages manuels de l'appareil, le passage au reflex devient comme une évidence.</Paragraph>
+            <Paragraph>Je passe donc au Nikon D90 en juillet 2009, en caisson Aquatica, toujours avec mon fidèle flash DS125, auquel je vais associer un DS160.</Paragraph>
+            <Paragraph>Après 5 années passés avec mon D90, j'ai ensuite évolué vers le D7100 puis le D7200, qui partagent tous les deux le même caisson Aquatica.</Paragraph>
+            <Paragraph>Entre temps, ce site est tout simplement né du désir de partager cette passion avec famille et amis, et c'est début 2022 que cette 6ème version a vu le jour...</Paragraph>
         </React.Fragment>
     )
 }
