56c15247f36fc39316271f638abbb6da035914f3
David Guillemet <david.guillemet@hotmail.com>
2022-09-08 20:22:42 +0200

    Fix destination link color

--- src/components/gallery/imageDestinationLink.js
+++ src/components/gallery/imageDestinationLink.js
@@ -6,7 +6,16 @@ import { useReactQuery } from '../reactQuery';
 
 const DestinationLinkContent = ({destination}) => {
     return (
-        <Typography variant="body2" sx={{ mt: 0.5, mb: 0}}>{destination.title} - {formatDateShort(new Date(destination.date))}</Typography>
+        <Typography
+            variant="body2"
+            sx={{
+                mt: 0.5,
+                mb: 0,
+                color: theme => theme.palette.link.main
+            }}
+        >
+            {destination.title} - {formatDateShort(new Date(destination.date))}
+        </Typography>
     );
 };
 
