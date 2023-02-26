55832a3be46f3c73588c114634d90454284dc01f
David Guillemet <david.guillemet@hotmail.com>
2023-02-26 18:37:50 +0100

    Customized theme

--- src/template/theme/themeProvider.js
+++ src/template/theme/themeProvider.js
@@ -3,6 +3,49 @@ import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
 import { useDarkMode } from './darkModeProvider';
 
+const paletteDarkMode = {
+    mode: 'dark',
+    link: {
+        main: "#28a745"
+    },
+    primary: {
+      main: '#2b6c7d',
+      contrastText: '#d0d8c6',
+    },
+    secondary: {
+      main: 'rgba(175,199,82,0.7)',
+      contrastText: '#d2d4c2',
+    },
+    error: {
+      main: '#de6f64',
+    },
+    success: {
+      main: '#6dd072',
+    },
+};
+
+const paletteLightMode = {
+    mode: 'light',
+    link: {
+        main: "#28a745"
+    },
+    primary: {
+      main: '#568e98',
+      contrastText: '#cdd8c2',
+    },
+    secondary: {
+      main: '#afc752',
+      contrastText: '#fdfde6',
+    },
+    error: {
+      main: '#de6f64',
+    },
+    success: {
+      main: '#6dd072',
+      contrastText: '#dbffdf'
+    },
+}
+
 const CustomThemeProvider = ({children}) => {
     
     const { darkMode } = useDarkMode();
@@ -10,12 +53,7 @@ const CustomThemeProvider = ({children}) => {
     const theme = React.useMemo(
         () => responsiveFontSizes(
             createTheme({
-                palette: {
-                    mode: darkMode ? 'dark' : 'light',
-                    link: {
-                        main: "#28a745"
-                    }
-                },
+                palette: darkMode ? paletteDarkMode : paletteLightMode,
                 pageWidth: {
                     maxWidth: '800px',
                     width: '98%'
