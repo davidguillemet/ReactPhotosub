006cde3ac11affd3a3ddef3cb0645c5a533c19c6
David Guillemet <david.guillemet@hotmail.com>
2023-09-05 23:38:38 +0200

    Translate page 404

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { PageTitle, PageHeader } from '../../template/pageTypography';
 import { VerticalSpacing } from '../../template/spacing';
 import { Helmet404 } from '../../template/seo';
+import { useTranslation } from 'utils';
 
 const NotFound = (props) => {
-
+    const t = useTranslation("pages.404");
     return (
         <React.Fragment>
             <Helmet404 />
-            <PageTitle>Oups...</PageTitle>
-            <PageHeader>Il semblerait que la page que vous avez demandée n'existe pas ou a été supprimée.</PageHeader>
+            <PageTitle>{t("title")}</PageTitle>
+            <PageHeader>{t("explanation")}</PageHeader>
             <VerticalSpacing factor={2} />
             <img alt="" src="/404.gif" />
         </React.Fragment>
