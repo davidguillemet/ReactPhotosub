52136a365ebfc9bcfc16ec50ccf5b084fb646574
David Guillemet <david.guillemet@hotmail.com>
2024-08-25 19:09:41 +0200

    Add uid in favorites get request to get favorites from another user (if admin)

--- functions/api/resources/favorites.js
+++ functions/api/resources/favorites.js
@@ -3,15 +3,24 @@ module.exports = function(app, config) {
     app.use("/favorites", config.isAuthenticated);
 
     // Get, Add and Delete favorites
-    app.route("/favorites")
+    app.route("/favorites/:uid")
         // Get all favorites for the current user
         // Since the react query cache is hydrated from userdata response
         // It would be used only in case /userdata failed...
         .get(async function(req, res, next) {
+            // The uid of the user for whom we are requesting the favorites.
+            // If it is an admin, it can request favorites for everybody.
+            // If not, it can request favorites for himself only
+            const requestFavoritesUid = req.params.uid;
+            const currentUid = res.locals.uid;
+            if (requestFavoritesUid !== currentUid && !config.isAdmin(res)) {
+                res.locals.errorMessage = "Un non-admin ne peut pas demander les favoris d'un autre utilisateur.";
+                next(new Error(res.locals.errorMessage), req, res);
+            }
             res.locals.errorMessage = "Le chargement des favoris a échoué.";
             return config.pool({i: "images"})
                 .select("i.id", "i.name", "i.path", "i.title", "i.description", "i.sizeRatio", "i.create", "i.tags")
-                .join(config.pool().raw(`user_data as u ON u.uid = '${res.locals.uid}' and concat(i.path, '/', i.name) = ANY(u.favorites)`))
+                .join(config.pool().raw(`user_data as u ON u.uid = '${requestFavoritesUid}' and concat(i.path, '/', i.name) = ANY(u.favorites)`))
                 .orderBy("i.create", "desc") // Last image first
                 .then((images) => {
                     images.forEach((image) => {
@@ -20,7 +29,10 @@ module.exports = function(app, config) {
                     });
                     res.json(images);
                 }).catch(next);
-        })
+        });
+
+    // Get, Add and Delete favorites
+    app.route("/favorites")
         // Add a favorite for a given user
         .post(async function(req, res, next) {
             const favorites = req.body; // Path array
