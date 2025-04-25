1709cd9824c68c0e346caadadb0187115227bf27
David Guillemet <david.guillemet@hotmail.com>
2025-04-25 20:17:48 +0200

    Fix prerender token

--- functions/index.js
+++ functions/index.js
@@ -35,6 +35,9 @@ exports.mainapi = onRequest({secrets: [
     reCaptchaV3SecretKey,
 ]}, mainapi);
 
+const prerenderTokenSecretKey = defineSecret("PRERENDER_TOKEN");
+
 exports.preRender = onRequest({secrets: [
     postgreSqlPassword,
+    prerenderTokenSecretKey,
 ]}, preRender);
